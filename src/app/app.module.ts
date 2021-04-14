6fab4514b896a3a05b54aeace5658c26d2e8863d
Luciano Novello <lucianonovello2007@hotmail.com>
2021-04-13 21:56:40 -0300

    Aprendiendo Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Projects } from './Projects/projects';
+
+import { ProjectComponent } from './project/project.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Projects,
+   
+    ProjectComponent
+   
   ],
   imports: [
     BrowserModule,
